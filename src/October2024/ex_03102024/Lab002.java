793ee13722c8ebe32689ea927a28b9b9ff032e6c
91967 <galipalliaravindsharma@gmail.com>
2024-10-04 06:47:02 +0530

    All files

---
+++ src/October2024/ex_03102024/Lab002.java
@@ -0,0 +1,11 @@
+package October2024.ex_03102024;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        int num = 10;
+        for (int i = 1; i <= 10; ++i) {
+            System.out.println(num+"*"+i+"="+(num*i));
+
+        }
+    }
+}
---
+++ src/October2024/ex_03102024/Lab003.java
@@ -0,0 +1,12 @@
+package October2024.ex_03102024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int Addition = a+b;
+        int Subtraction = a-b;
+        System.out.println("Sum of two numbers is  " + (Addition));
+        System.out.println("Minus of two numbers is  " + (Subtraction));
+    }
+}
